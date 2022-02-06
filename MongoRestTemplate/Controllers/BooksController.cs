c43c38fee8b69579f0822767b34e82af3cee5cc7
one6n <clod94@live.it>
2022-02-06 23:05:00 +0100

    Aggiungere i file di progetto.

---
+++ MongoRestTemplate/Controllers/BooksController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoRestTemplate.Services;
+using MongoRestTemplate.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MongoRestTemplate.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        private readonly BooksService _booksService;
+
+        public BooksController(ILogger<BooksController> logger, BooksService booksService)
+        {
+            _logger = logger;
+            _booksService = booksService;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public async Task<List<Book>> Get()
+        {
+            _logger.Log(LogLevel.Information, $"method={nameof(Get)}");
+
+            return await _booksService.GetAsync();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            _logger.Log(LogLevel.Information, $"method={nameof(Get)}, id={id}");
+
+            var book = await _booksService.GetAsync(id);
+            if (book == null)
+                return NotFound();
+
+            return book;
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            _logger.Log(LogLevel.Information, $"method={nameof(Post)}, newBook={newBook}");
+            await _booksService.CreateAsync(newBook);
+
+            return CreatedAtAction(nameof(Post), new { id = newBook.Id }, newBook);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Put(string id, Book updatedBook)
+        {
+            _logger.Log(LogLevel.Information, $"method={nameof(Put)}, id={id}, updatedBook={updatedBook}");
+
+            var book = await _booksService.GetAsync(id);
+            if (book == null)
+                return NotFound();
+
+            await _booksService.UpdateAsync(id, updatedBook);
+
+            return NoContent();
+
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            _logger.Log(LogLevel.Information, $"method={nameof(Delete)}, id={id}");
+
+            var book = await _booksService.GetAsync(id);
+            if (book == null)
+                return NotFound();
+
+            await _booksService.RemoveAsync(book.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ MongoRestTemplate/Services/BooksService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoRestTemplate.Models;
+
+namespace MongoRestTemplate.Services
+{
+    public class BooksService
+    {
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<Book> _booksCollection;
+
+        public BooksService(IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+        {
+            _client = new MongoClient(bookStoreDatabaseSettings.Value.ConnectionString);
+            _database = _client.GetDatabase(bookStoreDatabaseSettings.Value.DatabaseName);
+            _booksCollection = _database.GetCollection<Book>(bookStoreDatabaseSettings.Value.BooksCollectionName);
+        }
+        public async Task<List<Book>> GetAsync() =>
+            await _booksCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Book?> GetAsync(string id) =>
+            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Book newBook) =>
+            await _booksCollection.InsertOneAsync(newBook);
+
+        public async Task UpdateAsync(string id, Book updatedBook) =>
+            await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+        public async Task RemoveAsync(string id) =>
+            await _booksCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
